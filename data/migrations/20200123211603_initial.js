a8f474e07b551cd66c50fd329d4e9c4850be21ea
rogerLee <rmlee0003@gmail.com>
2020-01-23 21:21:06 -0500

    made migration

--- data/migrations/20200123211603_initial.js
+++ data/migrations/20200123211603_initial.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("cars",(table) => {
+      table.increments()
+      table.string("make").notNullable()
+      .unique()
+      .table.string("models").notNullable()
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.droptableIfExists("cars")
 };

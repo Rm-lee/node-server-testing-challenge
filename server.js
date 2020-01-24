f6050f48444bed51a6d1c72ce846035954d41278
rogerLee <rmlee0003@gmail.com>
2020-01-23 21:17:06 -0500

    added deps, knexfile, dbconfig

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const server = express()
 
-server.get('/api', (req,res) => {
+server.get('/', (req,res) => {
     res.status(200).json({message:"working"})
 })
 module.exports = server
